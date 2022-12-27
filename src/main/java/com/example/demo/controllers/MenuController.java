70b60ceb4b91cf7a529b3788e530a84e05da523f
mvatanasov18 <54852701+mvatanasov18@users.noreply.github.com>
2022-12-27 15:17:07 +0200

    Change style for label

--- src/main/java/com/example/demo/controllers/MenuController.java
+++ src/main/java/com/example/demo/controllers/MenuController.java
@@ -1,22 +1,23 @@
 package com.example.demo.controllers;
 
-import com.example.demo.models.Meal;
 import com.example.demo.repositories.MealRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.List;
-
+@RequestMapping(value = "/menus")
 @RestController
-public class HomeController {
-    @GetMapping(value = {"/","index"})
-    public ModelAndView getIndex(){
+public class MenuController {
+    @Autowired
+    private MealRepository repository;
+
+    @GetMapping()
+    public ModelAndView getMenus(){
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("index");
+        modelAndView.setViewName("menus");
+        modelAndView.addObject("meals",repository.findAll());
         return modelAndView;
     }
 }

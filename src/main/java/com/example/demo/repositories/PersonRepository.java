985b7297a0f216af737b46fabfd59000ba6c0856
Martin Atanasov <mvatanasov18@codingburgas.bg>
2023-01-12 23:33:49 +0200

    Fix #1

--- src/main/java/com/example/demo/repositories/PersonRepository.java
+++ src/main/java/com/example/demo/repositories/PersonRepository.java
@@ -1,12 +1,10 @@
 package com.example.demo.repositories;
 
-import com.example.demo.models.Meal;
 import com.example.demo.models.Person;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface PersonRepository extends CrudRepository<Person,Integer> {
-    Person findMealById(Integer id);
     List<Person> findAll();
 }
